2f1ca6f7f31ff2940ce41fdd2bd9fdb2c77cc234
NTanushri <139727279+NTanushri@users.noreply.github.com>
2023-07-17 14:28:48 +0530

    Create Isodd.java

---
+++ Isodd.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+class UserMainCode
+{
+  public int isOdd(int input1)
+  {
+    int n=input1;
+    if(n%2!=0)
+      return 2;
+    else
+      return 1;
+  }
+}
